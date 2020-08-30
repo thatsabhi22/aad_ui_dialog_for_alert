c98f77767215cbc986c2d8abd0b3f820b08ef91c
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-08-30 16:22:06 +0530

    AlertDialog Functionality Added

--- app/src/main/java/com/theleafapps/pro/dialogforalert/MainActivity.java
+++ app/src/main/java/com/theleafapps/pro/dialogforalert/MainActivity.java
@@ -1,14 +1,49 @@
 package com.theleafapps.pro.dialogforalert;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button alert_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alert_btn = findViewById(R.id.alert_btn);
+        alert_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myAlertBuilder =
+                        new AlertDialog.Builder(MainActivity.this);
+                myAlertBuilder.setTitle("Alert");
+                myAlertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
+                myAlertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Pressed OK",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                myAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                myAlertBuilder.show();
+            }
+        });
     }
 }
